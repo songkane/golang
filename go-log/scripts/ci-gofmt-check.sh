7881cd2dcb0de23cfa37a9bb700e023ff307b2e1
玄苦 <cgl1079743846@gmail.com>
2019-01-27 10:29:18 +0800

    golog rename 2 go-log

--- go-log/scripts/ci-gofmt-check.sh
+++ go-log/scripts/ci-gofmt-check.sh
@@ -1,26 +1,26 @@
-# Go语言源码中静态错误的简单工具
+# Go语言源码格式化
 #!/bin/bash
 
-echo '********** go vet check start ... **********'
+echo '********** gofmt check start ... **********'
 
-# check has error
+# check has illegal format files
 # mac下sed命令和linux有所区别
 # mac sed: sed -i 需要带一个字符串，用来备份源文件，这个字符串加在源文件名后面组成备份文件名
 # sed -i "bs" 's/Atl/Dog/g' example.txt 则会生成example.txtbs 的备份文件
 # sed -i "" 's/Atl/Dog/g' example.txt 如果这个字符串长度为0，就是说是个空串，那么不备份
-errors=`go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/golog/=./= | grep -v gitlab | xargs -n 1 go vet`
+errors=`go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-log/=./= | grep -v gitlab | xargs -n 1 gofmt -l`
 
 # echo errors
-echo ${errors}
+echo "${errors}"
 
 # grep errors has go file
 echo "${errors}" | grep "\.go"
 
-# $? equal 0 has error
+# $? equal 0 found check error
 if [ $? -eq 0 ]; then
-    echo '********** go vet check failed ~ **********'
+    echo '********** gofmt check failed ~ **********'
     exit 1
 else
-    echo '********** go vet check ok ~ **********'
+    echo '********** gofmt check ok ~ **********'
     exit 0
 fi
