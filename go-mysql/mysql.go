723d960a083e347a70cf4780c38ae7ca8d0132f4
玄苦 <cgl1079743846@gmail.com>
2019-01-28 14:28:38 +0800

    gomysql rename 2 go-mysql

--- go-mysql/mysql.go
+++ go-mysql/mysql.go
@@ -59,7 +59,7 @@ func newMasterDB(conf *Config) (*sql.DB, error) {
 
 	// open DB
 	// master DB
-	master, err := sql.Open(DefaultDriver, masterDsn)
+	master, err := sql.Open(sql.DefaultDriver, masterDsn)
 	if err != nil {
 		return nil, err
 	}
@@ -78,7 +78,7 @@ func newSlavesDB(conf *Config) ([]*sql.DB, error) {
 	// slaves DB
 	slaves := make([]*sql.DB, 0)
 	for _, slaveDsn := range slavesDsn {
-		db, err := sql.Open(DefaultDriver, slaveDsn)
+		db, err := sql.Open(sql.DefaultDriver, slaveDsn)
 		if err != nil {
 			return nil, err
 		}
