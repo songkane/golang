f59265a2e7f07d33f00bae2d1af02d08f91d1374
玄苦 <cgl1079743846@gmail.com>
2019-01-28 14:03:17 +0800

    httpserver rename 2 go-http

---
+++ go-http/instance/instance_test.go
@@ -0,0 +1,47 @@
+// Package instance unit test
+// Created by chenguolin 2018-11-16
+package instance
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestInitAppContext(t *testing.T) {
+	// TODO 用户需要自行修改路径
+	filePath := os.Getenv("GOPATH") + "/src/gitlab.local.com/golang/go-http/config/conf/config-pre.toml"
+	AppInit(filePath)
+}
+
+func TestInitTestContext(t *testing.T) {
+	AppInitTest()
+}
+
+func TestAddInitFunc(t *testing.T) {
+	f := func() { fmt.Println("hello word func1") }
+	AddInitFunc("func", f)
+
+	f2 := func() { fmt.Println("hello word func2") }
+	AddInitFunc("func2", f2)
+
+	// TODO 用户需要自行修改路径
+	filePath := os.Getenv("GOPATH") + "/src/gitlab.local.com/golang/go-http/config/conf/config-pre.toml"
+	AppInit(filePath)
+}
+
+func TestGetMysqlClient(t *testing.T) {
+	// TODO
+}
+
+func TestGetRedisClient(t *testing.T) {
+	// TODO
+}
+
+func TestGetMcClient(t *testing.T) {
+	// TODO
+}
+
+func TestGetKafkaConf(t *testing.T) {
+	// TODO
+}
