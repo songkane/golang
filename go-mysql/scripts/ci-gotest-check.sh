f1afbdc68f1b7a3a58afb5128d4b2bfb5edba872
玄苦 <cgl1079743846@gmail.com>
2019-01-28 14:30:56 +0800

    gomysql rename 2 go-mysql

--- go-mysql/scripts/ci-gotest-check.sh
+++ go-mysql/scripts/ci-gotest-check.sh
@@ -8,7 +8,7 @@ echo '********** go test check start ... **********'
 # mac sed: sed -i 需要带一个字符串，用来备份源文件，这个字符串加在源文件名后面组成备份文件名
 # sed -i "bs" 's/Atl/Dog/g' example.txt 则会生成example.txtbs 的备份文件
 # sed -i "" 's/Atl/Dog/g' example.txt 如果这个字符串长度为0，就是说是个空串，那么不备份
-errors=$(go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-mysql/=./= | grep -v gitlab | xargs -n 1 go test` 2>&1)
+errors=$(go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-mysql/=./= | grep -v gitlab | xargs -n 1 go test 2>&1)
 
 # grep errors has FAIL
 echo "${errors}" | grep "FAIL"
