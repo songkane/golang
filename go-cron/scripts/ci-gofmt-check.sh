37e485000eb605250dcd5b15b117cdd49c3e5747
玄苦 <cgl1079743846@gmail.com>
2019-01-28 13:06:29 +0800

    gocommon rename 2 go-common

--- go-cron/scripts/ci-gofmt-check.sh
+++ go-cron/scripts/ci-gofmt-check.sh
@@ -8,10 +8,8 @@ echo '********** gofmt check start ... **********'
 # mac sed: sed -i 需要带一个字符串，用来备份源文件，这个字符串加在源文件名后面组成备份文件名
 # sed -i "bs" 's/Atl/Dog/g' example.txt 则会生成example.txtbs 的备份文件
 # sed -i "" 's/Atl/Dog/g' example.txt 如果这个字符串长度为0，就是说是个空串，那么不备份
-errors=`go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-cron/=./= | grep -v gitlab | xargs -n 1 gofmt -l`
-
-# echo errors
-echo "${errors}"
+# 命令结果输出到变量errors，同时把标准错误也输出
+errors=$(go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-cron/=./= | grep -v gitlab | xargs -n 1 gofmt -l 2>&1)
 
 # grep errors has go file
 echo "${errors}" | grep "\.go"
@@ -19,6 +17,7 @@ echo "${errors}" | grep "\.go"
 # $? equal 0 found check error
 if [ $? -eq 0 ]; then
     echo '********** gofmt check failed ~ **********'
+    echo "${errors}"
     exit 1
 else
     echo '********** gofmt check ok ~ **********'
