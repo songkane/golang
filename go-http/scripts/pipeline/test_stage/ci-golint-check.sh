a5dad2394183d0232a9b48307aac01b20d4da31c
玄苦 <cgl1079743846@gmail.com>
2019-01-28 13:16:42 +0800

    httpserver rename 2 go-http

--- go-http/scripts/pipeline/test_stage/ci-golint-check.sh
+++ go-http/scripts/pipeline/test_stage/ci-golint-check.sh
@@ -8,10 +8,7 @@ echo '********** golint check start ... **********'
 # mac sed: sed -i 需要带一个字符串，用来备份源文件，这个字符串加在源文件名后面组成备份文件名
 # sed -i "bs" 's/Atl/Dog/g' example.txt 则会生成example.txtbs 的备份文件
 # sed -i "" 's/Atl/Dog/g' example.txt 如果这个字符串长度为0，就是说是个空串，那么不备份
-errors=`go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-http/=./= | grep -v gitlab | xargs -n 1 golint`
-
-# echo errors
-echo "${errors}"
+errors=$(go list ./... | grep -v vendor | sed -e s=gitlab.local.com/golang/go-http/=./= | grep -v gitlab | xargs -n 1 golint 2>&1)
 
 # grep errors has go file
 echo "${errors}" | grep "\.go"
@@ -19,6 +16,7 @@ echo "${errors}" | grep "\.go"
 # $? equal 0 found check error
 if [ $? -eq 0 ]; then
     echo '********** golint check failed ~ **********'
+    echo "${errors}"
     exit 1
 else
     echo '********** golint check ok ~ **********'
